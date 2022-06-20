eca528c6160fac18539e7dfdd79d436b36898886
soypat <graded.sp@gmail.com>
2022-06-20 18:33:10 -0300

    add append

---
+++ exercises/016-append/append.go
@@ -0,0 +1,32 @@
+// Problem:
+// append is a built-in that helps construct slices whose size is undefined
+// at runtime.
+//
+// To append an item to an existing slice one uses the append function
+//  short := []string{"first"}
+//  longer := append(short, "second")
+//
+// The result is stored to `longer` and is a slice of two elements, "first" followed
+// by "second".
+//
+// A common pattern is to append to the same slice
+// 
+//  elements = append(elements, "an item")
+//
+
+package main
+
+import "fmt"
+
+func main() {
+	var evens []int
+	for i := 1; i <= 20; i++ {
+		if i%2 == 0 {
+			// Knowing the basic mechanics of slices, how 
+			// would you go about constructing a slice of
+			// all even numbers from 1 up to 20?
+			evens = ??
+		}
+	}
+	fmt.Println(evens)
+}
