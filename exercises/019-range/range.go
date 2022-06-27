99db5c2cad7681d5d62adb0237ecb4d2e61f12c8
soypat <graded.sp@gmail.com>
2022-06-27 01:59:59 -0300

    add range and first draft of range2

---
+++ exercises/019-range/range.go
@@ -0,0 +1,40 @@
+// Problem:
+// A "for" statement can be written with a "range" clause
+// to iterate through all values in a slice, map and other types.
+//
+//  for i := range slice {
+//  	slice[i] = f()
+//  }
+//
+// The code above sets all items in the slice
+// to the value returned by calling the function f.
+//
+// It is also possible to add a second value to the range.
+//
+//  for i, v := range slice {
+//  	slice[i] = v * v
+//  }
+//
+// The code above squares all values in the slice.
+// Keep in mind the first value returned by the range
+// clause will be the nth index for slices. The second
+// value of the range clause is the nth value in the slice.
+//
+
+package main
+
+import "fmt"
+
+func main() {
+	// Dang nabit, this program was
+	// supposed to double the values in the slice
+	// but it's doing anything but that.
+	//
+	// Mind the order of index and value
+	// returned by the range clause.
+	slice := []int{1, 2, 3, 4}
+	for i, v := range slice {
+		slice[v] = 2 * i
+	}
+	fmt.Println(slice)
+}
---
+++ exercises/020-range2/range2.go
@@ -0,0 +1,26 @@
+// Problem:
+//
+
+package main
+
+import "fmt"
+
+func main() {
+	fruitScores := map[string]int{
+		"apple":     5,
+		"banana":    8,
+		"tangerine": 10,
+	}
+	// Why won't this compile!?
+	// Go is picky on unused variables.
+	// In this particular case we can reduce
+	// the overall code by quite a lot. Use
+	// v inside this for block to replace
+	// map accesses and print out fruits scoring
+	// higher than 7.
+	for k, v := range fruitScores {
+		if fruitScores[k] > 7 {
+			fmt.Println(k, " has score ", fruitScores[k])
+		}
+	}
+}
