cd8250ed4d644bcbcb80da0f16ea63afebde9de1
SRNissen <4851097+SRNissen@users.noreply.github.com>
2023-06-26 21:39:23 +0200

    Issue #9 Correction to explanatory text
    The two loops are not quite the same (the scope of var i is different) so let's avoid telling beginning learners that they are "identical"

--- exercises/014-for/for.go
+++ exercises/014-for/for.go
@@ -6,9 +6,9 @@
 //  	fmt.Println(i)
 //  }
 //
-// The above for loop will iterate from 0 to 10 and is
-// identical in functionality to the following
-// single-condition for statement:
+// The above for loop will iterate from 0 to 10.
+// The following single-condition for statement
+// will perform the same operation:
 //
 //  i := 0
 //  for i <= 10 {
